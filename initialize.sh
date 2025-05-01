5887663dbe104eb62d8834289f2914afd3a684af
jj <log@riseup.net>
2025-05-01 18:51:03 +0000

    build from git commit tarballs

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-LLVM_RELEASE="$(cat llvm-release.txt)"
-[ ! -f llvm-project-"$LLVM_RELEASE".src.tar.xz ] && curl -O -L https://github.com/llvm/llvm-project/releases/download/llvmorg-"$LLVM_RELEASE"/llvm-project-"$LLVM_RELEASE".src.tar.xz
-[ "$(shasum -a 512 llvm-project-"$LLVM_RELEASE".src.tar.xz)" != "$(cat checksum.txt)" ] && exit 1
+LLVM_COMMIT="$(cat llvm-release.txt)"
+[ ! -f "$LLVM_COMMIT.tar.gz" ] && curl -O -L https://github.com/llvm/llvm-project/archive/$LLVM_COMMIT.tar.gz
+[ "$(shasum -a 512 "$LLVM_COMMIT".tar.gz)" != "$(cat checksum.txt)" ] && exit 1
 mkdir final
-tar -xf llvm-project-"$LLVM_RELEASE".src.tar.xz
-mv llvm-project-"$LLVM_RELEASE".src final/llvm-project
+tar -xzf "$LLVM_COMMIT".tar.gz
+mv llvm-project-"$LLVM_COMMIT"/ final/llvm-project
 patch -p1 -R -d final/llvm-project < 4dec62f4d4a0a496a8067e283bf66897fbf6e598.patch
