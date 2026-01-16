d96d70b3b4a26b83bdc07cc1c40ff0499e886447
jj <log@riseup.net>
2026-01-16 22:32:33 +0000

    roll clang to ea10026b64f66b3b69c0545db20f9daa8579f5cb
    https://source.chromium.org/chromium/chromium/src/+/refs/tags/144.0.7559.59:tools/clang/scripts/update.py;bpv=1;bpt=0

--- initialize.sh
+++ initialize.sh
@@ -5,4 +5,3 @@ LLVM_COMMIT="$(cat llvm-release.txt)"
 mkdir final
 tar -xzf "$LLVM_COMMIT".tar.gz
 mv llvm-project-"$LLVM_COMMIT"/ final/llvm-project
-patch -p1 -R -d final/llvm-project < 4dec62f4d4a0a496a8067e283bf66897fbf6e598.patch
